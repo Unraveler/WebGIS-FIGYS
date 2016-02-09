01487eee06c5bdd4d233b1ad1da31941283ff9d4
Rui Nunes <rui.nunes1985@gmail.com>
2016-02-09 21:02:02 +0100

    Files commit
    File upload to repository

---
+++ data/json_FIGEvents.js
@@ -0,0 +1,15 @@
+var json_FIGEvents={
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+                                                                                
+"features": [
+{ "type": "Feature", "properties": { "Name": "Kuala Lumpur", "Date": "2014\/05\/01", "People": 23, "Countries": 14, "Event": null }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 101.686855, 3.139003, 0.0 ] ] } },
+{ "type": "Feature", "properties": { "Name": "Sofia", "Date": "2015\/05\/01", "People": 12, "Countries": 10, "Event": null }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 23.321868, 42.697708, 0.0 ] ] } },
+{ "type": "Feature", "properties": { "Name": "Abuja", "Date": "2013\/05\/01", "People": 15, "Countries": 1, "Event": null }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 7.398574, 9.076479, 0.0 ] ] } },
+{ "type": "Feature", "properties": { "Name": "Addis Ababa", "Date": "2014\/11\/01", "People": 45, "Countries": 10, "Event": null }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 38.757761, 8.980603, 0.0 ] ] } },
+{ "type": "Feature", "properties": { "Name": "Kumasi", "Date": "2013\/06\/01", "People": 60, "Countries": 1, "Event": null }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -1.616271, 6.6666, 0.0 ] ] } },
+{ "type": "Feature", "properties": { "Name": "Saint Lucia", "Date": null, "People": 26, "Countries": 12, "Event": null }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -60.978893, 13.909444, 0.0 ] ] } },
+{ "type": "Feature", "properties": { "Name": "Dakar", "Date": "2014\/05\/01", "People": 20, "Countries": 3, "Event": null }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -17.366029, 14.764504, 0.0 ] ] } },
+{ "type": "Feature", "properties": { "Name": "Kuala Lumpur", "Date": "2013\/09\/01", "People": 15, "Countries": 1, "Event": null }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 101.697747, 3.132431, 0.0 ] ] } }
+]
+}
